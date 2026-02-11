ffab36ddd8bf5cc88d3caa46add29078047c800b
Miles Josephson <jos21004@byui.edu>
2026-02-11 13:10:25 -0700

    here is the work for the week

---
+++ blog/blog.js
@@ -0,0 +1,11 @@
+{
+	id: 4,
+	title: "New Book",
+	date: "March 15, 2023",
+	description: "A new book that has been added to the collection.",
+	imgSrc: "https://example.com/new-book-cover.jpg",
+	imgAlt: "Cover of the new book",
+	ages: "10-16",
+	genre: "Adventure",
+	stars: "⭐⭐⭐⭐"
+}
